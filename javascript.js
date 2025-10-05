9cd8bdbbaf21b09a005f21dca31a50c4a09a1544
Lynx <129610092+gameraazvb@users.noreply.github.com>
2025-10-05 18:26:24 +0500

    Delete javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-function teachers(){
-    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1", "_blank");
-}
-function students(){
-    window.open("https://www.youtube.com/watch?v=bHSdGMenCfo&list=RDbHSdGMenCfo&start_radio=1", "_blank");
-}
-function credit(){
-    alert("created by Aaz(zen_dev)")
-}
