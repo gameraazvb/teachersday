e4975c388c8a11e6b6e41dd2e33e4e0374c46c74
Lynx <129610092+gameraazvb@users.noreply.github.com>
2025-10-05 18:26:43 +0500

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function teachers(){
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1", "_blank");
+}
+function students(){
+    window.open("https://www.youtube.com/watch?v=bHSdGMenCfo&list=RDbHSdGMenCfo&start_radio=1", "_blank");
+}
+function credit(){
+    alert("created by Aaz(zen_dev)")
+}
